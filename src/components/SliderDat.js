87a3eb8d407a865c17b25293c4488e860a9ff66d
Obokeng Molefe <obokengmolefe@Obokengs-MBP.lan>
2024-03-27 13:47:50 +0200

    first commit

---
+++ src/components/SliderDat.js
@@ -0,0 +1,11 @@
+export const SliderDat = [
+    {
+        image : 'https://images.unsplash.com/photo-1563252781-2f76e089e889?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80'
+    },
+    {
+        image : 'https://images.unsplash.com/photo-1508691643859-ca8898146a51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+    },
+    {
+        image : 'https://images.unsplash.com/photo-1541996275318-4890e83660a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80'
+    },
+];
